cdb70de673b0f36928ab6c06cd75e6f903a55920
Hammami Houssem <hammamihossem99@gmail.com>
2023-05-16 12:56:54 +0100

    up

--- scraping/views.py
+++ scraping/views.py
@@ -1,9 +1,16 @@
+from urllib.parse import parse_qs, urlparse
 from django.shortcuts import render
 import pandas as pd
 import requests
 from django.http import HttpResponse
 import time
 from bs4 import BeautifulSoup
+from django.core.cache import cache
+
+# ANSI escape codes for colors
+GREEN = "\033[92m"
+END = "\033[0m"
+YELLOW = "\033[33m"
 
 # dataframe colums
 columns = [
@@ -103,6 +110,17 @@ def generatePagination(pagination_html):
     return pages
 
 
+def getLastPage():
+    page = requests.get("https://www.jumia.com.tn/smartphones/")
+    soup = BeautifulSoup(page.content, "html.parser")
+    # Using CSS selector
+    a_last = soup.select_one('a[aria-label="Dernière page"]')
+    href = a_last["href"]
+    parsed_url = urlparse(href)
+    page = parse_qs(parsed_url.query).get("page")
+    return page[0]
+
+
 def productsList(query, page, max_price, min_price):
     """
     Fetches products from jumia.com.tn based on a query.
@@ -113,77 +131,71 @@ def productsList(query, page, max_price, min_price):
     """
     # Initialize an empty products DataFrame
     products_df = pd.DataFrame(columns=columns)
+    last_page = getLastPage()
 
     # Add the query from the user to fetch jumia for a specific product, and a page
-    params = {
-        "q": query,
-        "page": page,
-        "price": (min_price or "") + "-" + (max_price or ""),
-    }
+    for i in range(1, int(last_page)):
+        print("{}Srapping page number {}: {}".format(GREEN, i, END))
+        params = {
+            "page": i,
+        }
 
-    # Request jumia html page
-    page = requests.get(
-        "https://www.jumia.com.tn/telephones-smartphones/", params=params
-    )
+        # Request jumia html page
+        page = requests.get("https://www.jumia.com.tn/smartphones/", params=params)
 
-    # Pass the page to the BeautifulSoup library to handle it and extract information
-    soup = BeautifulSoup(page.content, "html.parser")
+        # Pass the page to the BeautifulSoup library to handle it and extract information
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    # Extract pages from jumia pagination page URLs
-    pagination = soup.find("div", {"class": "pg-w -ptm -pbxl"})
-    pages = generatePagination(pagination)
+        articles = soup.find_all("article", class_="prd")
 
-    articles = soup.find_all("article", class_="prd")
+        for article in articles:
+            # Extract article (product) details using BeautifulSoup methods (brand, name, price, image ...)
+            image = article.find("img", class_="img")
+            core = article.find("a", class_="core")
+            new_price = article.find("div", class_="prc")
+            old_price = article.find("div", class_="old")
+            sale = article.find("div", class_="bdg _dsct _sm")
+            officiel = article.find("div", class_="bdg _mall _xs")
 
-    for article in articles:
-        # Extract article (product) details using BeautifulSoup methods (brand, name, price, image ...)
-        image = article.find("img", class_="img")
-        core = article.find("a", class_="core")
-        new_price = article.find("div", class_="prc")
-        old_price = article.find("div", class_="old")
-        sale = article.find("div", class_="bdg _dsct _sm")
-        officiel = article.find("div", class_="bdg _mall _xs")
+            # Filter products by category to only add phones
+            if "Phones & Tablets/Mobile Phones/Smartphones" in core["data-category"]:
+                item = {
+                    "id": core["data-id"],
+                    "image": image["data-src"],
+                    "url": core["href"],
+                    "name": core["data-name"],
+                    "brand": core["data-brand"],
+                    "categorie": core["data-category"],
+                    "new_price": new_price.text if new_price else 0,
+                    "old_price": old_price.text if old_price else 0,
+                    "sale": sale.text if sale else 0,
+                    "officiel": 1 if officiel else 0,
+                }
 
-        # Filter products by category to only add phones
-        if "Phones & Tablets/Mobile Phones/Smartphones" in core["data-category"]:
-            item = {
-                "id": core["data-id"],
-                "image": image["data-src"],
-                "url": core["href"],
-                "name": core["data-name"],
-                "brand": core["data-brand"],
-                "categorie": core["data-category"],
-                "new_price": new_price.text if new_price else 0,
-                "old_price": old_price.text if old_price else 0,
-                "sale": sale.text if sale else 0,
-                "officiel": 1 if officiel else 0,
-            }
+                # Remove comma or dot from price strings and convert to float ('1,299.90 TND' to 1299.90)
+                item["new_price"] = float(item["new_price"].split()[0].replace(",", ""))
 
-            # Remove comma or dot from price strings and convert to float ('1,299.90 TND' to 1299.90)
-            item["new_price"] = float(item["new_price"].split()[0].replace(",", ""))
+                # sometime old price is none so we need to test it first
+                if item["old_price"]:
+                    item["old_price"] = float(
+                        item["old_price"].split()[0].replace(",", "")
+                    )
 
-            # sometime old price is none so we need to test it first
-            if item["old_price"]:
-                item["old_price"] = float(item["old_price"].split()[0].replace(",", ""))
+                # Add current item to the products DataFrame
+                item_df = pd.DataFrame([item], columns=products_df.columns)
+                products_df = pd.concat([products_df, item_df], ignore_index=True)
 
-            # Add current item to the products DataFrame
-            item_df = pd.DataFrame([item], columns=products_df.columns)
-            products_df = pd.concat([products_df, item_df], ignore_index=True)
+                # Convert "sale" column to numeric values
+                products_df["sale"] = (
+                    products_df["sale"].replace("%", "", regex=True).astype(float)
+                )
 
-            # Convert "sale" column to numeric values
-            products_df["sale"] = (
-                products_df["sale"].replace("%", "", regex=True).astype(float)
-            )
+                # Sort the DataFrame by "sale" column in descending order
+                products_df = products_df.sort_values(by="sale", ascending=False)
 
-            # Sort the DataFrame by "sale" column in descending order
-            products_df = products_df.sort_values(by="sale", ascending=False)
-
-    response = {
-        "products": products_df.to_dict(orient="records") or [],
-        "pages": pages or [],
-    }
-
-    return response
+    # save products in cach
+    cache.set("scraped_data", products_df.to_dict(orient="records") or [])
+    return products_df.to_dict(orient="records") or []
 
 
 def Smartphones(request):
@@ -204,8 +216,16 @@ def Smartphones(request):
     min_price = request.GET.get("min_price")
     max_price = request.GET.get("max_price")
 
-    # Initialize empty data if query is not set, otherwise fill it with products from jumia by query
-    data = productsList(query, page, max_price, min_price)
+    # Initialize data and fill it with products from jumia by query
+    data = cache.get("scraped_data")
+    if data is None:
+        print("{}Start scraping jumia pages:{}".format(YELLOW, END))
+        data = productsList(query, page, max_price, min_price)
+
+    # Filter products by brand and price
+    filtered_results = filter_products(
+        data, brand=query, min_price=min_price, max_price=max_price
+    )
 
     # Capture time in seconds
     timer.stop()
@@ -213,8 +233,7 @@ def Smartphones(request):
 
     # Return data to the Django template
     context = {
-        "products": data.get("products"),
-        "pagination": data.get("pages") or None,
+        "products": filtered_results,
         "query": query or "",
         "page": page,
         "min_price": min_price or "",
@@ -223,3 +242,28 @@ def Smartphones(request):
     }
 
     return render(request, "smartphones.html", context)
+
+
+def filter_products(products, brand=None, min_price=None, max_price=None):
+    if brand is None and min_price is None and max_price is None:
+        # No filters specified, return the full list of products
+        return products
+
+    filtered_products = []
+
+    for product in products:
+        # Check if brand matches the filter
+        if brand and product["brand"].lower() != brand.lower():
+            continue
+
+        # Check if price falls within the specified range
+        if min_price and min_price != "" and product["new_price"] < float(min_price):
+            continue
+
+        if max_price and max_price != "" and product["new_price"] > float(max_price):
+            continue
+
+        # Add the product to the filtered list
+        filtered_products.append(product)
+
+    return filtered_products
